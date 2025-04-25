4f43de0956d239cf41b821c989a6562b10602c3a
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-04-25 08:00:29 -0600

    Ejercicio 2

---
+++ Ejercicios_Python5/ejercicio 2.py
@@ -0,0 +1,14 @@
+salario = float(input("Ingrese su salario estimado trabajador: "))
+
+if salario > 3000:
+    bono = salario * 0.10
+elif salario > 1500 and salario <= 3000:
+    bono = salario * 0.05
+elif salario <= 1500:
+    print ("Usted no tendrá bono")
+else:
+    bono = 0
+
+print(f"{"Salario: ":<19} {salario:<3}")    
+print(f"{"Bono: ":<19} {bono:<3.2f}")
+print(f"{"Sueldo total: ":<19} {salario + bono:<3.2f}")
